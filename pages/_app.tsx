90ef0fb461fa0b46657ba22deca7c1010023d02f
Aravind Balla <bsaaravind@gmail.com>
2020-11-09 19:53:45 +0530

    try now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import GoogleFonts from "next-google-fonts";
 
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
@@ -6,5 +7,10 @@ import "prismjs/themes/prism-tomorrow.css";
 import "../styles.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,600;1,400&display=swap" />
+      <Component {...pageProps} />
+    </>
+  );
 }
