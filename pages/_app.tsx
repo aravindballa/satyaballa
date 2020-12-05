a3d525e2e2fe204e47134e1460d89bc947cbe4dd
Aravind Balla <bsaaravind@gmail.com>
2020-12-05 21:25:04 +0530

    feat: add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <script async src="https://unpkg.com/thesemetrics@latest"></script>
+        <script async defer data-domain="satyaballa.com" src="https://plausible.io/js/plausible.js"></script>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>"
